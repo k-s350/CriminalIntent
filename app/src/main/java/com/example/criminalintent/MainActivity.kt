1b2cce10c43b491d2b0e8c4ff2c1088ae81fc728
Ko <k_s350@student.usc.edu.au>
2026-09-04 11:39:39 +1000

    Implemented chapter 9: Fragment and database foundation

--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // replaced R.id.main with R.id.fragment_container didn't explain this in textbook seems to work tho
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
