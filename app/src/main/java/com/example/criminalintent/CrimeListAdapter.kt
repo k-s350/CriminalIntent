304c451a51550de3bc2790404baca5d4902c90e6
Ko <k_s350@student.usc.edu.au>
2026-09-23 15:30:56 +1000

    Implemented chapter 11

--- app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -34,6 +35,12 @@ class CrimeHolder(
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
         binding.crimeDate.text = crime.date.toString()
+
+        binding.crimeSolved.visibility = if (crime.isSolved) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
 
         binding.root.setOnClickListener {
             Toast.makeText(
